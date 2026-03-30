e55f2fb626557c6b609dd882cee3c2dc1b6b83d7
abcdefucky0 <ammaralqarobi363@gmail.com>
2026-03-30 21:12:16 +0800

    membuat image slider menggunakan viewpager2

--- app/src/main/java/com/yourname/aplikasitrackingpendayagunaan/MainActivity.kt
+++ app/src/main/java/com/yourname/aplikasitrackingpendayagunaan/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.viewpager2.widget.ViewPager2
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +17,16 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val images = listOf(
+            R.drawable.dot,
+            R.drawable.dot,
+            R.drawable.dot,
+
+        )
+
+        val viewPager = findViewById<ViewPager2>()
+
+
     }
 }
---
+++ app/src/main/java/com/yourname/aplikasitrackingpendayagunaan/SliderAdapter.kt
@@ -0,0 +1,29 @@
+package com.yourname.aplikasitrackingpendayagunaan
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+
+class SliderAdapter(private val images: List<Int>) :
+        RecyclerView.Adapter<SliderAdapter.SliderViewHolder>() {
+
+    inner class  SliderViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val  imageView: ImageView = view.findViewById(android.R.id.icon)
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SliderViewHolder {
+        val  view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_slider, parent , false)
+        return SliderViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: SliderViewHolder, position: Int) {
+        holder.imageView.setImageResource(images[position])
+    }
+
+    override fun getItemCount() = images.size
+
+        }
