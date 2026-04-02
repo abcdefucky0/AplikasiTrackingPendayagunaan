5d1b492a272e2cd859953bf4b08934db2bc9147c
abcdefucky0 <ammaralqarobi363@gmail.com>
2026-04-02 20:39:36 +0800

    pembuatan recycler view  di model dan adapter

---
+++ app/src/main/java/com/yourname/aplikasitrackingpendayagunaan/adapter/CampaignAdapter.java
@@ -0,0 +1,10 @@
+package com.yourname.aplikasitrackingpendayagunaan.adapter;
+
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+public class CampaignAdapter {
+}
---
+++ app/src/main/java/com/yourname/aplikasitrackingpendayagunaan/model/CampaignModel.kt
@@ -0,0 +1,20 @@
+package com.yourname.aplikasitrackingpendayagunaan.model
+
+data class CampaignModel(
+    val id: Int,
+    val slug: String,
+    val title: String,
+    val description: String,
+    val donasiterkumpul: Int?,
+    val start: String?,
+    val end: String?,
+    val target: Int?,
+    val status: String?,
+    val image: String?,
+    val category_id: Int?,
+    val user_id: Int?,
+    val create_at: String?,
+    val update_at: String?,
+    val agency_id: Int?,
+    val delete_at: String?
+    )
