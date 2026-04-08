2e20fb0bbf7a321c7df9f6a32542b922fa9f7942
abcdefucky0 <ammaralqarobi363@gmail.com>
2026-04-08 20:36:33 +0800

    pembuatan bot nav

--- app/src/main/java/com/yourname/aplikasitrackingpendayagunaan/MenuTracking.kt
+++ app/src/main/java/com/yourname/aplikasitrackingpendayagunaan/MenuTracking.kt
@@ -2,6 +2,7 @@ package com.yourname.aplikasitrackingpendayagunaan
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -48,4 +49,5 @@ class MenuTracking : AppCompatActivity() {
             insets
         }
     }
+
 }
