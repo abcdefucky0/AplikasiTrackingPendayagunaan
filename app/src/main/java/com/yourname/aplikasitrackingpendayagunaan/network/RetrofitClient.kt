1581638fb3990671d0aa3d6014d4667e06ecf8ce
abcdefucky0 <ammaralqarobi363@gmail.com>
2026-07-30 19:11:35 +0800

    Tugas TA

--- app/src/main/java/com/yourname/aplikasitrackingpendayagunaan/network/RetrofitClient.kt
+++ app/src/main/java/com/yourname/aplikasitrackingpendayagunaan/network/RetrofitClient.kt
@@ -7,8 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
 
-    const val BASE_URL = "https://siazistracking.free.nf/bakti_bersama/"
-
+    const val BASE_URL = "https://brilliant-avenge-unbroken.ngrok-free.dev/bakti_bersama/"
     private val loggingInterceptor = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
     }
@@ -20,6 +19,7 @@ object RetrofitClient {
             val request = original.newBuilder()
                 .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36")
                 .header("Accept", "application/json")
+                .header("ngrok-skip-browser-warning", "true")
                 .method(original.method, original.body)
                 .build()
             chain.proceed(request)
